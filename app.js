dcb6a0fb2ba486e01275d3c20bf18148c765878e
esseppidefi <97457479+esseppidefi@users.noreply.github.com>
2022-03-09 15:18:52 +0100

    still probs

--- app.js
+++ app.js
@@ -1,68 +1,107 @@
 let container = document.querySelector('#container');
-let icon = document.querySelector('.post-meta__icon');
-let btnAddLike = document.querySelector('.like-button');
-let likeCounter = document.querySelector('#like-counter-1');
+for (let i in posts) {
+  generatePost(posts[i]);
+}
 
-function generatePost() {
-  for (let i = 0; i < posts.length; i++) {
-    let element = posts[i];
-    let id = element.id;
-    let content = element.content;
-    let media = element.media;
-    let name = element.author.name;
-    let image = element.author.image;
-    let photo = `<img class="profile-pic" src="${image}" alt="${name}">`;
-    let likes = element.likes;
-    let created = element.created;
-    let post = document.createElement('div');
-    if (image == null) {
-      let rgx = new RegExp(/(\p{L}{1})\p{L}+/, 'gu');
-      let initials = [...name.matchAll(rgx)] || [];
-      initials = (
-        (initials.shift()?.[1] || '') + (initials.pop()?.[1] || '')
-      ).toUpperCase();
-      photo = `<div>${initials}</div>`;
+function generatePost(ele) {
+  let post = document.createElement('div');
+  post.classList.add('post');
+  post.dataset.postid = ele.id;
+  post.innerHTML = `
+    <div class="post">
+      <div class="post__header">
+          <div class="post-meta">
+              <div class="post-meta__icon">
+                  ${getProfilePicHtml(ele)}
+              </div>
+              <div class="post-meta__data">
+                  <div class="post-meta__author">${ele.author.name}</div>
+                  <div class="post-meta__time">${dateFromIsoToItalian(
+                    ele.created
+                  )}</div>
+              </div>
+          </div>
+      </div>
+      <div class="post__text">${ele.content}</div>
+      <div class="post__image">
+          <img src="${ele.media}" alt="">
+      </div>
+      <div class="post__footer">
+          <div class="likes js-likes">
+              <div class="likes__cta">
+                  <a class="like-button  js-like-button" href="#" data-postid="${
+                    ele.id
+                  }">
+                      <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                      <span class="like-button__label">Mi Piace</span>
+                  </a>
+              </div>
+              <div class="likes__counter">
+                  Piace a <b id="like-counter-1" class="js-likes-counter">${
+                    ele.likes
+                  }</b> persone
+              </div>
+          </div>
+      </div>
+    </div>
+  `;
+  post.querySelector('.js-like-button').addEventListener('click', toggleLike);
+  container.appendChild(post);
+}
+function getProfilePicHtml(ele) {
+  if (ele.author.image == null) {
+    let arrNameParts = ele.author.name.split(' ');
+    let initials = '';
+    for (let index in arrNameParts) {
+      initials += arrNameParts[index][0].toUpperCase();
     }
-    post.classList.add('team-card');
-    post.innerHTML = `
-        <div class="post">
-        <div class="post__header">
-            <div class="post-meta">
-                <div class="post-meta__icon">
-                    ${photo}
-                </div>
-                <div class="post-meta__data">
-                    <div class="post-meta__author">${name}</div>
-                    <div class="post-meta__time">${created}</div>
-                </div>
-            </div>
-        </div>
-        <div class="post__text">${content}</div>
-        <div class="post__image">
-            <img src="${media}" alt="">
-        </div>
-        <div class="post__footer">
-            <div class="likes js-likes">
-                <div class="likes__cta">
-                    <a class="like-button  js-like-button" href="#" data-postid="${id}">
-                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                        <span class="like-button__label">Mi Piace</span>
-                    </a>
-                </div>
-                <div class="likes__counter">
-                    Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
-                </div>
-            </div>
-        </div>
-      </div>`;
-
-    container.appendChild(post);
+    return `<div class="profile-pic-default"><span>${initials}</span></div>`;
+  } else {
+    return `<img class="profile-pic" src="${ele.author.image}" alt="${ele.author.name}"></img>`;
   }
 }
-generatePost();
+function dateFromIsoToItalian(originalDate) {
+  return originalDate.split('-').reverse().join('/');
+}
 
-btnAddLike.addEventListener('submit', addLike());
+function toggleLike(event) {
+  event.preventDefault();
+  const btnLike = this;
+  const elePost = btnLike.closest('.post');
+  const postId = parseInt(elePost.dataset.postid);
+  const eleCounter = elePost.querySelector('.js-likes-counter');
 
-function addLike(event) {
-  // crea oggetto del nuovo membro prendendo i dati dal form
+  let indexLikedPost = 0;
+  while (postId != posts[indexLikedPost].id) {
+    indexLikedPost++;
+  }
+  const objPost = arrPosts[indexLikedPost];
+
+  if (btnLike.classList.contains('like-button--liked')) {
+    removeLike(btnLike, objPost);
+  } else {
+    addLike(btnLike, objPost);
+  }
+
+  eleCounter.innerHTML = objPost.likes;
+}
+
+function removeLike(btnLikeArgument, objPostArgument) {
+  btnLikeArgument.classList.remove('like-button--liked');
+  objPostArgument.likes--;
+  const index = arrLikedIds.indexOf(objPostArgument.id);
+  arrLikedIds.splice(index, 1);
+
+  console.log(arrLikedIds);
+  console.table(arrPosts);
+}
+
+function addLike(btnLikeArgument, objPostArgument) {
+  btnLikeArgument.classList.add('like-button--liked');
+
+  objPostArgument.likes++;
+  arrLikedIds.push(objPostArgument.id);
+
+  console.log(arrLikedIds);
+  console.table(arrPosts);
 }
